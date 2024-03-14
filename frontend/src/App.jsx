72e1517a3be1df2420b6824e1dfb49b838aa6c36
Taha <tahamulla@hotmail.com>
2024-03-14 17:01:17 -0400

    Refactor modal functionality in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,22 @@
+// App.jsx
 import React, { useState } from "react";
 import "./App.scss";
 import HomeRoute from "./routes/HomeRoute";
-import PhotoDetailsModal from "./routes/PhotoDetailsModal"; 
+import PhotoDetailsModal from "./routes/PhotoDetailsModal"; // Import the PhotoDetailsModal component
 import photos from "./mocks/photos";
 import topics from "./mocks/topics";
 
 const App = () => {
-  const [displayModal, setDisplayModal] = useState(false); // State for modal visibility
+  const [displayModal, setDisplayModal] = useState(false);
 
   const closeModal = () => {
-    setDisplayModal(false); // Close the modal
+    setDisplayModal(false);
   };
 
   return (
     <div className="App">
       <HomeRoute photos={photos} topics={topics} setDisplayModal={setDisplayModal} />
-      {displayModal && <PhotoDetailsModal closeModal={closeModal} />} {/* Conditionally render modal */}
+      {displayModal && <PhotoDetailsModal closeModal={closeModal} />}
     </div>
   );
 };
