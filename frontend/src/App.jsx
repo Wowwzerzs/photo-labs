4fa8fe5bbe9a01defe2578898d0837164234f2ca
Taha <tahamulla@hotmail.com>
2024-03-14 11:39:34 -0400

    Added a NavBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import PhotoList from './components/PhotoList';
-import TopicList from './components/TopicList';
-import TopNavigation from './components/TopNavigationBar';
-import './App.scss';
+import TopicList from "./components/TopicList";
+import TopNavigation from "./components/TopNavigationBar";
+import "./App.scss";
 
 const App = () => (
   <div className="App">
     <TopNavigation />
-    <TopicList />
     <PhotoList />
   </div>
 );
--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import FavIcon from './FavIcon';
-
-import '../styles/FavBadge.scss';
+import React from "react";
+import FavIcon from "./FavIcon";
+import "../styles/FavBadge.scss";
 
 const FavBadge = ({ isFavPhotoExist }) => {
   return (
-    <div className='fav-badge'>
+    <div className="fav-badge">
       <FavIcon displayAlert={!!isFavPhotoExist}/>
     </div>
   ) 
