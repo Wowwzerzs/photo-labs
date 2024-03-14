e2f6b66fc5c6d22de8cc7af9fa1466e3301111f2
Taha <tahamulla@hotmail.com>
2024-03-14 03:56:11 -0400

    Refactored code

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -9,11 +9,11 @@ const PhotoListItem = (props) => {
   return (
     <div className="photo-item">
       <img src={imageSource} alt="Photograph" />
+      <img src={profile} alt="Profile" className="profile-pic" />
       <div className="details">
         <div>{username}</div>
         <div>{location}</div>
       </div>
-      <img src={profile} alt="Profile" className="profile-pic" />
     </div>
   );
 };
